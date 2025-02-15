2369a28a2efdf2b69f12b946acc34d9f6539335d
belar <bel.arth92@gmail.com>
2025-02-15 13:23:08 +0300

    third block

--- works/02_second_module/003_Third_block/01_first.py
+++ works/02_second_module/003_Third_block/01_first.py
@@ -15,7 +15,7 @@
 user_name = input('Имя: ')
 user_order = int(input('Номер заказа: '))
 
-answer = "Здравстуйте, {name}! Ваш номер заказа: {order}".format(name=user_name, order = user_order)
+answer = "Здравстуйте, {name}! Ваш номер заказа: {order}".format(user_name,user_order)
 print(answer)
 
 # Задача 2. Долги
@@ -32,7 +32,11 @@ print(answer)
 
 # Том! Том, привет! Как дела, Том? Где мои 100 рублей? Том! 
 
+user_name = input('Введите имя: ')
+user_debt = int(input('Введите долг: '))
 
+sentence = "{0}! {0}, привет! Как дела, {0}? Где мои {1} рублей? {0}!".format(user_name, user_debt)
+print(sentence)
 
 # Задача 3. IP-адрес
 # IP-адрес компьютера состоит из 4 чисел, разделённых точкой. Каждое число находится в диапазоне от 0 до 255 (включительно).
@@ -47,3 +51,19 @@ print(answer)
 
 # Напишите программу, которая получает на вход 4 числа и выводит на экран IP-адрес. Используйте переменную ip_address в качестве шаблона.
 #  Обеспечьте контроль ввода.
+
+ip_adress = []
+for _ in range(4):
+    while True:  # Повторяем ввод, пока не получим корректное число
+        try:
+            ip_num = int(input('Введите число от 0 до 255: '))
+            if 0 <= ip_num <= 255:
+                ip_adress.append(ip_num)
+                break  # Выход из цикла при успешном вводе
+            else:
+                print('Неверное число, попробуйте снова.')
+        except ValueError:
+            print('Ошибка! Введите целое число.')
+
+ip_final = "{0}.{1}.{2}.{3}".format(*ip_adress)
+print("Ваш IP-адрес:", ip_final)
