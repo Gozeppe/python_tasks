5aa3c403db3c9290cc77cf562a83d47b3704b60a
Gozeppe <bel.arth92@gmail.com>
2025-07-04 16:34:07 +0300

    fifth_block from second module

--- works/02_second_module/004_Fourth_block/03_third.py
+++ works/02_second_module/004_Fourth_block/03_third.py
@@ -5,6 +5,15 @@
 # Напишите (модифицируйте) программу, которая реализует этот алгоритм шифрования. Не используйте конкатенацию и сделайте так,
 # чтобы текст был в одном регистре.
 
+text = input("Введите текст для шифрования: ").lower()
+k = int(input("Введите сдвиг: "))
+
+result = ''.join([
+    chr((ord(char) - ord('a') + k) % 26 + ord('a')) if char.isalpha() else char
+    for char in text
+])
+
+print("Зашифрованный текст:", result)
 
 
 # Задача 2. Путь к файлу
@@ -13,7 +22,12 @@
 # Пользователь вводит абсолютный путь к текстовому файлу, а также проверяемые данные: диск и расширение файла. Напишите программу, 
 # которая проверяет корректность этого пути.
 
- 
+file_path = input('Введите путь к файлу: ')
+file_true_path = input('На каком диске:')
+file_extension = input('Требуемое расширение файла: ')
+
+if file_path.startswith(file_true_path.upper() + ":") and file_path.endswith(file_extension):
+    print('Путь корректен!')
 
 # Пример:
 
@@ -33,15 +47,24 @@
 
 # Подсказка: используйте методы islower() и/или isupper().
 
- 
+user_text = input('Введите строку: ')
 
-# Пример:
+upper_text = [letter for letter in user_text if letter.isupper()]
+lower_text = [letter for letter in user_text if letter.islower()]
+
+if len(upper_text) > len(lower_text):
+    print(user_text.upper()) 
+elif len(lower_text) > len(upper_text):
+    print(user_text.lower())
+else:
+    print('Количество нижних и верхних равно')
 
 # Введите строку: ПитоН - этО хорошО
 
  
 
 # Результат: питон - это хорошо
+
 
  
 
